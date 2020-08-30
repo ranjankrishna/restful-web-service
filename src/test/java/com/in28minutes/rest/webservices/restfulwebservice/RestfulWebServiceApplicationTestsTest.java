f76930072cede9c6dec846b61afeea52bf0956ba
Krishna Ranjan <ranjan.krishna@gmail.com>
2020-08-30 12:33:10 +0530

    first commit

---
+++ src/test/java/com/in28minutes/rest/webservices/restfulwebservice/RestfulWebServiceApplicationTestsTest.java
@@ -0,0 +1,11 @@
+package com.in28minutes.rest.webservices.restfulwebservice;
+
+import junit.framework.TestCase;
+
+public class RestfulWebServiceApplicationTestsTest extends TestCase {
+
+	public void testContextLoads() {
+		fail("Not yet implemented");
+	}
+
+}
